5edcc20b2c7ca931738b4cf0fb6bb3bdf755f1a4
Maverick Tesini <mavericktesini@gmail.com>
2018-08-01 22:23:57 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name=(name)
-    name = Artist.find_or_create_by(name: name)
+    artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
